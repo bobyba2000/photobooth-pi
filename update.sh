60219c055f6e2de8d37367e4d39a572f3c81e3e4
DatTT58 <bobyba20@gmail.com>
2026-03-13 18:20:03 +0700

    Update readme

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/pi/Desktop || exit
+
+# Pull latest code
+if /usr/bin/git pull origin main | grep -q "Already up to date."; then
+    echo "No updates found"
+else
+    echo "Update detected, restarting service"
+    sudo systemctl restart server.service
+fi
