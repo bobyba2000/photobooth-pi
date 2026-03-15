74fb225dcd1d26e7c7b1baac53f987cf3638b0d9
DatTT58 <bobyba20@gmail.com>
2026-03-15 18:00:03 +0700

    Fix sh

--- update.sh
+++ update.sh
@@ -2,10 +2,9 @@
 
 cd /home/pi/photobooth-pi || exit
 
-# Pull latest code
 if /usr/bin/git pull origin main | grep -q "Already up to date."; then
     echo "No updates found"
 else
     echo "Update detected, restarting service"
-    sudo systemctl restart server.service
+    sudo /bin/systemctl restart server.service
 fi
